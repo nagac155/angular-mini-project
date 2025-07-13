8b2877ec589a3839ec30305d71b48744c0065288
Naga Tummala <naga.c155@gmail.com>
2025-07-13 15:01:56 -0400

    error handling

--- src/app/harry-potter-spells/harry-potter-spells.component.ts
+++ src/app/harry-potter-spells/harry-potter-spells.component.ts
@@ -18,11 +18,21 @@ export class CatsComponent implements OnInit, OnDestroy {
   private petsService = inject(PetsService);
   spells = signal<HPSpell[]>([]);
   private subscription = new Subject<void>();
+  error = signal<string | null>(null);
+  loading = signal<boolean>(true);
 
   ngOnInit(): void {
-      this.petsService.getSpells().pipe(takeUntil(this.subscription)).subscribe((e: HPSpell[]) => {
-        console.log(e)
-        this.spells.set(e);
+    this.petsService.getSpells()
+      .pipe(takeUntil(this.subscription))
+      .subscribe({
+        next: (e: HPSpell[]) => {
+          this.loading.set(false);
+          this.spells.set(e);
+      },
+        error: (error) => {
+          this.loading.set(false);
+            this.error.set('Failed to load spells. Please try again later.' + (error?.message ? `Error: ${error.message}` : ''));
+          }
       });
   }
 
